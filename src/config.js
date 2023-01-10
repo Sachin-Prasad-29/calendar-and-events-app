4827a672aeed713c7aba246d527f195fc5c9f38c
Sachin kumar prasad <skpkorba9009@gmail.com>
2023-01-11 00:44:10 +0530

    url cnhange

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-// const baseURL = 'https://calender-and-event.onrender.com/api/'
+const baseURL = 'https://calender-and-event.onrender.com/api/'
 // const baseURL = 'https://calendar-and-events-server.vercel.app/api/'
 // const baseURL = 'https://calendar-and-events.herokuapp.com/api/';
-const baseURL = 'http://localhost:5001/api/';
+// const baseURL = 'http://localhost:5001/api/';
 export { baseURL }
